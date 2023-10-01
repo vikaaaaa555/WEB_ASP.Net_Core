8f018bc763b6bf311afd023189616ae2b0c69b41
Victoryia Zviahintsava <vika.zv04@yandex.ru>
2023-10-01 18:11:33 +0300

    The lab1 was done.

---
+++ WEB_153503_Zviagintsava/WEB_153503_Zviagintsava/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153503_Zviagintsava.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ WEB_153503_Zviagintsava/WEB_153503_Zviagintsava/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153503_Zviagintsava.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
