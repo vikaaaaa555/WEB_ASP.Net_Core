c4afa309c92b391d43f6b1892272d76c7130e758
Victoryia Zviahintsava <vika.zv04@yandex.ru>
2023-10-19 21:05:10 +0300

    Lab2 created

--- WEB_153503_Zviagintsava/WEB_153503_Zviagintsava/Controllers/Home.cs
+++ WEB_153503_Zviagintsava/WEB_153503_Zviagintsava/Controllers/Home.cs
@@ -6,7 +6,24 @@ namespace WEB_153503_Zviagintsava.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Message"] = "Лабораторная работа №2";
             return View();
         }
-	}
+
+        public class ListDemo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public ActionResult MenuPartial()
+        {
+            return PartialView("_MenuPartial");
+        }
+
+        public ActionResult UserInfoPartial()
+        {
+            return PartialView("_UserInfoPartial");
+        }
+    }
 }
---
+++ WEB_153503_Zviagintsava/WEB_153503_Zviagintsava/ViewsComponents/Cart.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153503_Zviagintsava.ViewsComponents
+{
+    public class Cart : ViewComponent
+    {
+        private double cost = 0.0;
+        public string Invoke() => $"{cost:N2} руб";
+    }
+}
